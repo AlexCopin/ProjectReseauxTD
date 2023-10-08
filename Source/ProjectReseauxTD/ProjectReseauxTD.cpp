8a069f17cf3633919c1b515ce3ed518e9f564714
AlexCopin <alex.copin@outlook.com>
2023-10-08 17:34:47 +0200

    Project CPP

---
+++ Source/ProjectReseauxTD/ProjectReseauxTD.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ProjectReseauxTD.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectReseauxTD, "ProjectReseauxTD" );
