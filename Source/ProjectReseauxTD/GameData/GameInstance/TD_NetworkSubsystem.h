7dc42bb3419af7a31cdef7f4827623ac67793f46
AlexCopin <alex.copin@outlook.com>
2023-10-09 11:24:52 +0200

    Change server port

---
+++ Source/ProjectReseauxTD/GameData/GameInstance/TD_NetworkSubsystem.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include <enet6/enet.h>
+#include "TD_NetworkSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTRESEAUXTD_API UTD_NetworkSubsystem : public UGameInstanceSubsystem, public FTickableGameObject
+{
+	GENERATED_BODY()
+	
+public:
+	ETickableTickType GetTickableTickType() const override;
+	bool IsAllowedToTick() const override final;
+	void Tick(float DeltaTime) override;
+	TStatId GetStatId() const override;
+
+	UFUNCTION(BlueprintCallable)
+	bool Connect(FString AddressString);
+
+	UFUNCTION(BlueprintCallable)
+	void Disconnect();
+
+	void Initialize(FSubsystemCollectionBase& Collection) override;
+
+	void Deinitialize() override;
+
+private:
+	ENetHost* Host = nullptr;
+	ENetPeer* ServerPeer = nullptr;
+};
